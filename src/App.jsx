880cb58c36923371ee65fa5db4e09c809e7571e4
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 17:34:21 +0300

    feat: create main App component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/**
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
+* Date: August 2026
+* Description:
+* Main React component for the My Tasks application.
+*/
+
+function App() {
+    return (
+        <main>
+            <h1>My Tasks</h1>
+        </main>
+    );
+}
+
+export default App;
