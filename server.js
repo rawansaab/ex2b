9d535b6b496ed663468de907781a37fee0d8e687
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 21:11:28 +0300

    feat: enable JSON requests on server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 /**
-* Names: Rawan Saab (213693625), Lareen Kadour (213992431),
-* George Hanna (324090968)
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
 * Date: August 2026
 * Github URL: https://github.com/rawansaab/ex2b
 * Description:
 * Main server file for Exercise 2B.
-* The server will handle user login, registration,
+* The server handles user login, registration,
 * personal task data and communication with the database.
 *
 * Imported modules:
@@ -16,6 +15,8 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
