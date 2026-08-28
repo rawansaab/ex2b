c6ecd8119b04bcd3987fedabc7e067a4ccaf10c5
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 23:28:39 +0300

    feat: create tasks component

---
+++ src/Tasks.jsx
@@ -0,0 +1,16 @@
+/**
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
+* Date: August 2026
+* Description:
+* React component for displaying the user's personal tasks.
+*/
+
+function Tasks() {
+    return (
+        <section>
+            <h2>My Tasks</h2>
+        </section>
+    );
+}
+
+export default Tasks;
