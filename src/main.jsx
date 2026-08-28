55906fbbf34e7fbb17e0bcd63692cc2525b9e7df
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 16:50:17 +0300

    feat: create React main file

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+/**
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
+* Date: August 2026
+* Description:
+* Main React file that connects the application to the root element.
+*/
+
+import { createRoot } from "react-dom/client";
