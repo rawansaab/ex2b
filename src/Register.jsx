3664a8877bea05f38e8b3d19f71038ca744e3551
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 19:23:49 +0300

    feat: create Register component

---
+++ src/Register.jsx
@@ -0,0 +1,16 @@
+/**
+* Names: Rawan Saab (213693625), Lareen Kadour (213992431), George Hanna (324090968)
+* Date: August 2026
+* Description:
+* React component for the user registration screen.
+*/
+
+function Register() {
+    return (
+        <section>
+            <h2>Register</h2>
+        </section>
+    );
+}
+
+export default Register;
