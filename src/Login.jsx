a2c16439f3a58012d73857579141497376858e8a
Rawan Saab <rawansaab777@gmail.com>
2026-08-28 18:50:32 +0300

    feat: add login form

--- src/Login.jsx
+++ src/Login.jsx
@@ -9,6 +9,26 @@ function Login() {
     return (
         <section>
             <h2>Login</h2>
+
+            <form>
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    required
+                />
+
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                />
+
+                <button type="submit">Login</button>
+            </form>
         </section>
     );
 }
